b4f758567bc83e1e0517b2fcf5aec74b7f9ab13b
Nicolas Antunes <nicolas.aoliveira100@gmail.com>
2024-09-09 09:46:09 -0300

    Criado funções e editado style

---
+++ main.js
@@ -0,0 +1,47 @@
+
+window.revelar = ScrollReveal({reset:true});
+
+
+
+
+revelar.reveal('.start-text', {
+    duration: 2000,
+    
+});
+
+
+
+
+revelar.reveal('.case1-effect', {
+    duration: 1000,
+    distance: '90px'
+});
+
+revelar.reveal('.case2-effect', {
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.case3-effect', {
+    duration: 3000,
+    distance: '90px'
+});
+
+
+
+revelar.reveal('.about-us-box', {
+    duration: 1000,
+    distance: '90px'
+});
+
+
+
+
+
+
+
+
+
+
+
+
